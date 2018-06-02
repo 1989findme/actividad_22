a8af48b3349f5fcf9bf1e6bb5c1d1ca4a29d4cb9
emanuel.perez.astorga@gmail.com <emanuel.perez.astorga@gmail.com>
2018-06-01 20:44:33 -0400

    cuarto commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_31_060252) do
+ActiveRecord::Schema.define(version: 2018_06_01_044141) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
